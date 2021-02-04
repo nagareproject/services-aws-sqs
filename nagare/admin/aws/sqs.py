185444db2f6cea2d5a57f599aa6aff719a6fab34
Alain Poirier <alain.poirier@net-ng.com>
2021-02-04 20:38:20 +0100

    Initial checkin

---
+++ nagare/admin/aws/sqs.py
@@ -0,0 +1,104 @@
+# --
+# Copyright (c) 2008-2020 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+import time
+
+from nagare.admin import command
+
+
+class Commands(command.Commands):
+    DESC = 'AWS SQS subcommands'
+
+
+class Receive(command.Command):
+    WITH_STARTED_SERVICES = True
+    DESC = 'receive data from a queue'
+
+    def __init__(self, name, dist, **config):
+        super(Receive, self).__init__(name, dist, **config)
+        self.nb = 0
+
+    def set_arguments(self, parser):
+        parser.add_argument('queue', help='queue service')
+
+        super(Receive, self).set_arguments(parser)
+
+    def handle_request(self, msg):
+        print('- {} --------------------'.format(self.nb))
+
+        print('Id: {}'.format(msg.message_id))
+
+        if msg.attributes:
+            print('Attributes:')
+
+            padding = len(max(msg.attributes, key=len))
+            for k, v in sorted(msg.attributes.items()):
+                print(' - {}: {}'.format(k.ljust(padding), v))
+
+        if msg.message_attributes:
+            print('Message attributes:')
+
+            padding = len(max(msg.message_attributes, key=len))
+            for k, v in sorted(msg.message_attributes.items()):
+                print(' - {}: {}'.format(k.ljust(padding), v))
+
+        print('Body: {}'.format(msg.body))
+
+        self.nb += 1
+        print('')
+
+        msg.delete()
+
+    def run(self, queue, services_service):
+        queue = services_service[queue]
+        print('Listening on <{}>...'.format(queue.name))
+
+        queue.start_consuming(self.handle_request, AttributeNames=['All'], MessageAttributeNames=['All'])
+
+        return 0
+
+
+class Send(command.Command):
+    WITH_STARTED_SERVICES = True
+    DESC = 'send data to a queue'
+
+    def set_arguments(self, parser):
+        parser.add_argument('-g', '--group', help='MessageGroupId')
+        parser.add_argument(
+            '-a', '--attr', action='append',
+            metavar=('name', 'value'), nargs=2,
+            help='message attributes'
+        )
+        parser.add_argument(
+            '-l', '--loop', action='store_true',
+            help='infinite loop sending <data> each 2 seconds'
+        )
+
+        parser.add_argument('queue', help='queue service')
+        parser.add_argument('data', help='data to send')
+
+        super(Send, self).set_arguments(parser)
+
+    @staticmethod
+    def run(loop, queue, group, data, attr, services_service):
+        queue = services_service[queue]
+        attrs = {name: {'StringValue': value, 'DataType': 'String'} for name, value in (attr or ())}
+
+        try:
+            while True:
+                params = {'MessageGroupId': group} if group else {}
+                queue.send_message(MessageBody=data, MessageAttributes=attrs, **params)
+                time.sleep(1)
+
+                if not loop:
+                    break
+        except KeyboardInterrupt:
+            pass
+
+        return 0
---
+++ nagare/services/aws/sqs.py
@@ -0,0 +1,124 @@
+# Encoding: utf-8
+
+# --
+# Copyright (c) 2008-2020 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+"""Provides classes to interact with the AWS SQS service"""
+
+import re
+from concurrent.futures import ThreadPoolExecutor
+
+from nagare.services import plugin
+from nagare.services.aws.resources import AWS
+
+
+class SQS(plugin.Plugin):
+    """
+    """
+    LOAD_PRIORITY = AWS.LOAD_PRIORITY + 1
+
+    def __init__(
+            self,
+            name, dist,
+            aws_service,
+            services_service,
+            **config
+    ):
+        services_service(plugin.Plugin.__init__, self, name, dist, **config)
+        self.sqs = aws_service.create_resource('sqs')
+
+    @staticmethod
+    def to_camelcase(identifier):
+        return re.sub('_(.)', lambda m: m.group(1).upper(), '_' + identifier)
+
+    @property
+    def queues(self):
+        return self.sqs.queues
+
+    def create_queue(self, queue_name, tags=None, **kw):
+        attributes = {
+            self.to_camelcase(name): str(value).lower() if isinstance(value, bool) else str(value)
+            for name, value in kw.items()
+            if value is not None
+        }
+
+        return self.sqs.create_queue(QueueName=queue_name, Attributes=attributes, tags=tags or {})
+
+    def get_queue(self, queue_name, account_id=None):
+        params = {'QueueOwnerAWSAccountId': account_id} if account_id is not None else {}
+        return self.sqs.get_queue_by_name(QueueName=queue_name, **params)
+
+
+class Queue(plugin.Plugin):
+    LOAD_PRIORITY = SQS.LOAD_PRIORITY + 1
+    CONFIG_SPEC = dict(
+        plugin.Plugin.CONFIG_SPEC,
+        queue_name='string',
+        account_id='string(default=None)',
+        pool='integer(default=1)',
+
+        creation='boolean(default=False)',
+        fifo_queue='boolean(default=None)',
+        delay_seconds='integer(default=0)',
+        maximum_message_size='integer(default=262144)',
+        message_retention_period='integer(default=345600)',
+        receive_message_wait_time_seconds='integer(default=0)',
+        redrive_policy='string(default=None)',
+        visibility_timeout='integer(default=30)',
+        content_based_deduplication='boolean(default=None)',
+        deduplication_scope='option(messageGroup, queue, default=None)',
+        fifo_throughput_limit='option(perMessageGroupId, perQueue, default=None)',
+
+        tags={'__many__': 'string(default=None)'}
+    )
+
+    def __init__(self, name, dist, queue_name, account_id, pool, creation, tags, sqs_service, **config):
+        super(Queue, self).__init__(
+            name, dist,
+            queue_name=queue_name, account_id=account_id,
+            pool=pool,
+            creation=creation, tags=tags,
+            **config
+        )
+
+        if creation:
+            queue = sqs_service.create_queue(queue_name, tags, **config)
+        else:
+            queue = sqs_service.get_queue(queue_name, account_id)
+
+        self.queue = queue
+        self.pool = pool
+
+    @property
+    def dead_letter_source_queues(self):
+        return self.dead_letter_source_queues
+
+    def start_consuming(self, callback, **kw):
+        with ThreadPoolExecutor(self.pool) as executor:
+            while True:
+                for msg in self.receive_messages(**kw):
+                    executor.submit(callback, msg).add_done_callback(lambda f: f.result())
+
+
+for method in (
+    'add_permission',
+    'change_message_visibility_batch',
+    'delete',
+    'delete_messages',
+    'get_available_subresources',
+    'load',
+    'purge',
+    'receive_messages',
+    'reload',
+    'remove_permission',
+    'send_message',
+    'send_messages',
+    'set_attributes'
+):
+    setattr(Queue, method, lambda self, *, _method=method, **kw: getattr(self.queue, _method)(**kw))
